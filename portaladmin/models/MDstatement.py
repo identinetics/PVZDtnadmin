6987f8f1a07648c361a506a665a8d9aa33a96ac5
rhoerbe <rainer@hoerbe.at>
2018-12-02 11:16:18 +0100

    added unique constraint for entityid per statusgroup

--- portaladmin/models/MDstatement.py
+++ portaladmin/models/MDstatement.py
@@ -1,9 +1,11 @@
+import hashlib
 import json
 import tempfile
 from django.conf import settings
 from django.db import models
 
 from ..constants import *
+from django.core.exceptions import ValidationError
 from PVZDpy.policystore import PolicyStore
 from PVZDpy.samled_validator import SamlEdValidator
 from fedop.models.namespace import Namespaceobj
@@ -19,19 +21,18 @@ class MDstatementAbstract(models.Model):
         abstract = True
         ordering = ['-updated_at']
         verbose_name = 'Metadaten Statement'
+        unique_together = (('entityID', 'make_blank_entityid_unique', 'statusgroup'), )
 
     def __init__(self, *args, **kw):
         super(MDstatementAbstract, self).__init__(*args, **kw)
         self._ed_uploaded_old = self.ed_uploaded
-        self._ed_signed_old = self.ed_signed
-        self._ed_file_upload_name_old = self.ed_file_upload.name
 
     admin_note = models.TextField(
         blank=True, null=True,
         verbose_name='Admin Notiz',
         max_length=1000)
     content_valid = models.BooleanField(
-        default=False, null=True,
+        default=False, null=False,
         verbose_name='Content validation', )
     deletionRequest = models.BooleanField(
         default=False, null=True,
@@ -45,7 +46,7 @@ class MDstatementAbstract(models.Model):
         help_text='SAML EntityDescriptor (signiert)',
         max_length=100000)
     ed_uploaded = models.TextField(
-        blank=True, null=True,
+        blank=False, null=False,
         verbose_name='EntityDescriptor hochgeladen',
         help_text='SAML EntityDescriptor (geladen, nicht signiert)',
         max_length=100000)
@@ -60,6 +61,11 @@ class MDstatementAbstract(models.Model):
         blank=True, null=True,
         verbose_name='Entity FQDN',
         max_length=300)
+    make_blank_entityid_unique =models.CharField(
+        verbose_name = 'if the entityID is blank due to a validation error, this hash value '
+                       'assures that the unique constraint is not violated',
+        blank=True, null=True,
+        max_length=16)
     operation = models.CharField(
         blank=True, null=True,
         max_length=7)
@@ -80,15 +86,19 @@ class MDstatementAbstract(models.Model):
         max_length=80)
     status = models.CharField(
         verbose_name='Workflow Status',
-        default=STATUS_CREATED, null=True,
+        default=STATUS_CREATED, null=False,
         choices=STATUS_CHOICES,
         max_length=14)
+    statusgroup = models.CharField(
+        verbose_name='Status Group', null=False,
+        choices=STATUSGROUP_CHOICES,
+        max_length=8)
     validation_message  = models.TextField(
         blank=True, null=True,
         verbose_name='Error Messages',
         max_length=100000)
     namespace = models.CharField(
-        default=STATUS_CREATED, null=True,
+        blank=True, null=True,
         max_length=30)
 #    namespaceobj = models.ForeignKey(
 #        Namespaceobj,
@@ -114,8 +124,8 @@ class MDstatementAbstract(models.Model):
 
     def get_boilerplate_help(self):
         return "Ein Metadaten Statement wird erstellt und geändert, indem eine Datei mit einem " \
-               "SAML Entity Descriptor hochgeladen wird. Für die Signatur ist in der Listansicht " \
-               "der Eintrag zu markieren und die Aktion zum signieren auszuführen."
+               "SAML Entity Descriptor hochgeladen wird. Für die Signatur ist in der " \
+               "Listansicht der Eintrag zu markieren und die Aktion zum signieren auszuführen."
     get_boilerplate_help.short_description = ''
 
 
@@ -153,25 +163,28 @@ class MDstatementAbstract(models.Model):
             self.ed_val.validate_entitydescriptor(ed_str_new=self.ed_uploaded, sigval=False)
         pass
 
-    def save(self, *args, **kwargs):
+    def clean(self):
         def _fail_if_updating_not_allowed():
             if self.status == STATUS_ACCEPTED:
-                raise ValidationError(_('Cannot change if status = ' + STATUS_ACCEPTED), code='invalid')
+                raise ValidationError('Cannot change if status = ' + STATUS_ACCEPTED,
+                                      code='invalid')
 
-        def _read_uploaded_file_on_change():
-            if self.ed_file_upload:
-                ed_file_upload_name_new = self.ed_file_upload.file.name or ''
-                if self._ed_file_upload_name_old != ed_file_upload_name_new:
-                    self.ed_uploaded = self.ed_file_upload.file.read().decode('utf-8')
-                    self.ed_signed = None
-                elif not self.ed_uploaded:  # add
-                    self.ed_uploaded = self.ed_file_upload.file.read().decode('utf-8')
+        def _require_upload_for_change():
+            if not self.ed_file_upload.name:
+                raise ValidationError('Ein neuer EntityDescriptor muss hochgeladen werden')
+
+        _fail_if_updating_not_allowed()
+        _require_upload_for_change()
+
+    def save(self, *args, **kwargs):
+        def _read_uploaded_file():
+            if self.ed_file_upload.name:
+                self.ed_uploaded = self.ed_file_upload.file.read().decode('utf-8')
 
         def _set_status_on_upload():
-            if self.ed_uploaded:
-                if self.ed_uploaded != self._ed_uploaded_old:
-                    if self.status in (STATUS_CREATED, STATUS_REJECTED):
-                        self.status = STATUS_UPLOADED
+            if self.ed_uploaded != self._ed_uploaded_old:
+                if self.status in (STATUS_CREATED, STATUS_REJECTED):
+                    self.status = STATUS_UPLOADED
 
         def _set_computed_fields():
             self.validate()
@@ -180,22 +193,33 @@ class MDstatementAbstract(models.Model):
             self.ed_uploaded_filename = self.ed_file_upload.file.name if self.ed_file_upload else None
             self.entity_fqdn = self._get_fqdn()
             self.entityID = self._get_entityID()
+            self.make_blank_entityid_unique = self._get_make_blank_entityid_unique()
             self.namespace = self._get_namespace()
             self.operation = self._get_operation()
             self.org_cn = self._get_orgcn()
             self.org_id = self._get_orgid()
             self.signer_authorized = self._is_authorized()
             self.signer_subject = getattr(self.ed_val, 'signer_cert_cn', '')
+            self.statusgroup = self._get_statusgroup()
             self.validation_message = self._get_validation_message()
 
         if kwargs.get('operation', '') == 'mds_sign_and_update':
             super().save(*args, {})
+        elif kwargs.get('operation', '') == 'PEP':   # see security considerations in docs/
+            super().save(*args, {})
         else:
-            _fail_if_updating_not_allowed()
-            _read_uploaded_file_on_change()
+            _read_uploaded_file()
             _set_status_on_upload()
             _set_computed_fields()
             super().save(*args, **kwargs)
+
+    def _get_make_blank_entityid_unique(self):
+        if self.entityID:
+            return None
+        else:
+            hashobj = hashlib.md5()
+            hashobj.update(self.ed_uploaded)
+            return hashobj.digest()
 
     def _get_entityID(self):
         eid = self.ed_val.entityID
@@ -233,6 +257,12 @@ class MDstatementAbstract(models.Model):
     def _get_orgcn(self):
         if getattr(self.ed_val, 'ed', False):
             return self.ed_val.policystore.get_orgcn(self._get_orgid())
+
+    def _get_statusgroup(self):
+        if self.status == STATUS_ACCEPTED:
+            return STATUSGROUP_BACKEND
+        else:
+            return STATUSGROUP_FRONTEND
 
     def _get_validation_message(self):
         if getattr(self.ed_val, 'val_mesg_dict', False):
