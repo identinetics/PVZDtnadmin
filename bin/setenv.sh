e322c79dec6ffb68404c6d041b40d3aa36ffbfeb
rhoerbe <rainer@hoerbe.at>
2018-12-28 07:53:25 +0100

    fix pyjnius 1.1.4 upgrade issue (unset DYLD_LIBRARY_PATH)

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -7,9 +7,10 @@ export APP_HOME=$(dirname $scriptsdir)
 if [[ "$(uname)" == "Linux" ]] && [[ -f /etc/redhat-release ]]; then
     [[ "$JAVA_HOME" ]] || export JAVA_HOME=/etc/alternatives/java_sdk_1.8.0
 elif [[ "$(uname)" == "Darwin" ]]; then  # MacOS (Development)
-    [[ "$JAVA_HOME" ]] || export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0/Contents/Home
+    [[ "$JAVA_HOME" ]] || export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
     # Java 9 not yet supported (class loader problem?)
-    export DYLD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+    #export DYLD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+    #export DYLD_LIBRARY_PATH=$JAVA_HOME/lib/server
 elif [[ ! "$JAVA_HOME" ]]; then
     echo "Critical error: JAVA_HOME not set"
     exit 1
---
+++ docs/runtime_env.sh
@@ -0,0 +1,6 @@
+PYTHONPATH=$PROJ_HOME:$PROJ_HOME/PVZDlib
+CLASSPATH=/Users/admin/devl/python/identinetics/PVZDweb/PVZDlib/MOA-SPSS/moa-sig-lib-latest.jar:/Users/admin/devl/python/identinetics/PVZDweb/PVZDlib/MOA-SPSS/lib/*:/Users/admin/devl/python/identinetics/PVZDweb/PVZDlib/unittests/junit-4.11.jar:/Users/admin/devl/python/identinetics/PVZDweb/PVZDlib/PVZDjava/*
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0/Contents/Home  # /usr/libexec/java_home -v 1.8
+DJANGO_SETTINGS_MODULE=pvzdweb.settings_test
+# pyjnius<=1.1.3/macOS
+# DYLD_LIBRARY_PATH=/Library/Java/JavaVirtualMachines/jdk1.8.0/Contents/Home/jre/lib/server;
