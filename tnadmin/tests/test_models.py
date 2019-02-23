4e53568260add268b863d7d094506fcc166bbb53
rhoerbe <rainer@hoerbe.at>
2019-02-23 20:15:20 +0100

    fedop.tests.tetmodels now independet of persistent db (sqllite in-memorydb)

--- tnadmin/tests/test_models.py
+++ tnadmin/tests/test_models.py
@@ -1,17 +1,41 @@
+import os
+import pathlib
 import pytest
 from tnadmin.models import *
+import django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pvzdweb.settings_pytest_dev")
+django.setup()
+from django.conf import settings
+from django.core import management
+from pvzdweb.settings import *
+INSTALLED_APPS=list(set(INSTALLED_APPS + ['fedop']))
 
-pytestmark = pytest.mark.django_db
+#pytestmark = pytest.mark.django_db  # not working for whatever reason.
+                                     # workaround from https://github.com/pytest-dev/pytest-django/issues/396
+from pytest_django.plugin import _blocking_manager
+from django.db.backends.base.base import BaseDatabaseWrapper
+_blocking_manager.unblock()
+_blocking_manager._blocking_wrapper = BaseDatabaseWrapper.ensure_connection
 
 
-def test_gvorg_count():
+@pytest.fixture(scope="module")
+def setup_and_load_tnadmin1():
+    management.call_command('migrate', 'tnadmin')
+    tnadmin_data = pathlib.Path('tnadmin/fixtures/tnadmin1.json')
+    assert tnadmin_data.is_file(), f'could not find file {tnadmin_data}'
+    management.call_command('loaddata', tnadmin_data)
+
+
+def test_gvorg_count(setup_and_load_tnadmin1):
     assert len(GvOrganisation.objects.all()) > 0, 'No gvOrganisation data found'
 
-def test_gvorg_get():
+
+def test_gvorg_get(setup_and_load_tnadmin1):
     o = GvOrganisation.objects.get(gvouid='AT:B:1')
     assert o.gvouvkz == 'BMJ'
 
-def test_gvFedOrg_insert():
+
+def test_gvFedOrg_insert(setup_and_load_tnadmin1):
     o = GvOrganisation.objects.get(gvouid='AT:B:1')
     fedorg = GvFederationOrg(gvouid=o)
     fedorg.gvContractStatus = LEGAL_BASIS_PVV
